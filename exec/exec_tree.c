e84490ff1dda0b346b474d85cc5f0bee1f4c17df
vivaz-ca <vivaz-ca@student.42lisboa.com>
2025-10-09 17:34:02 +0100

    fixed the execution stuff issue, command is founf now

--- exec/exec_tree.c
+++ exec/exec_tree.c
@@ -6,7 +6,7 @@
 /*   By: vivaz-ca <vivaz-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 15:55:08 by vvazzs            #+#    #+#             */
-/*   Updated: 2025/10/09 17:00:20 by vivaz-ca         ###   ########.fr       */
+/*   Updated: 2025/10/09 17:32:00 by vivaz-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,18 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void	exec_child(t_cmds *cmd)
+int super_checker_goated(t_cmds *cmd, char *command, char **args)
+{
+	if (!has_builtin(cmd) && !has_redir(cmd) && !is_system_path_command(cmd, args))
+	{
+		print_cmds(cmd);
+		if (strchr(cmd->cmd[0], '/'))
+			handle_absolute_path_cmd(cmd->cmd[0], args, args);
+		return (ft_free_matrix(args), 1);
+	}
+	return (0);
+}
+void	exec_child(t_cmds *cmd)	
 {
 	char	**cleaned;
 	char	**updated_envs;
@@ -26,16 +37,23 @@ void	exec_child(t_cmds *cmd)
 	signal(SIGINT, handle_sigint);
 	cleaned = array_to_exec(cmd);
 	updated_envs = list_to_char(*get_env_list());
+
 	if (has_redir(cmd))
 		exec_redirections(cmd);
+
 	if (has_builtin(cmd))
 		exec_builtin(cleaned[0], cleaned, updated_envs);
+		
 	if (is_system_path_command(cleaned[0], updated_envs))
 		exec_path(cleaned[0], cleaned, updated_envs);
-	if (!has_redir(cmd))
-		my_ffprintf(cleaned[0], "command not found\n");
-	ft_free_matrix(cleaned);
-	ft_free_matrix(updated_envs);
+		
+	if (super_checker_goated(cmd, cleaned[0], updated_envs) != 0)
+		my_ffprintf(cmd->cmd[0], "command not found\n");
+		
+	if (cleaned)
+		ft_free_matrix(cleaned);
+	if (updated_envs)
+		ft_free_matrix(updated_envs);
 	megalodon_giga_chad_exit(127);
 }
 
