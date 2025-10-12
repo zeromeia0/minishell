187bef1ad8894976433964000b9373e1e861de2d
viniarch <insanecoderbr06@gmail.com>
2025-10-12 23:23:21 +0100

    ft matrix

--- exec/my_libft/libft/ft_free_matrix.c
+++ exec/my_libft/libft/ft_free_matrix.c
@@ -6,7 +6,7 @@
 /*   By: vvazzs <vvazzs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 14:46:11 by jlima-so          #+#    #+#             */
-/*   Updated: 2025/09/29 21:38:04 by vvazzs           ###   ########.fr       */
+/*   Updated: 2025/10/12 23:22:22 by vvazzs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_free_matrix(char **matrix)
 
 	ind = -1;
 	if (!matrix)
-		return ; //VINI ADDED THIS CHECK 
+		return ;
 	while (matrix[++ind])
 		free(matrix[ind]);
 	free (matrix);
