b648aae8fadbb160229fc72dafc0181e47cb543f
vivaz-ca <vivaz-ca@student.42lisboa.com>
2025-10-13 16:45:45 +0100

    fixed error

--- exec/exec_tree.c
+++ exec/exec_tree.c
@@ -6,7 +6,7 @@
 /*   By: vivaz-ca <vivaz-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 15:55:08 by vvazzs            #+#    #+#             */
-/*   Updated: 2025/10/13 16:39:26 by vivaz-ca         ###   ########.fr       */
+/*   Updated: 2025/10/13 16:44:11 by vivaz-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int super_checker_goated(t_cmds *cmd, char *command, char **args)
-{
-	if (!has_builtin(cmd) && !has_redir(cmd) && !is_system_path_command(cmd, args))
-	{
-		print_cmds(cmd);
-		if (strchr(cmd->cmd[0], '/'))
-			handle_absolute_path_cmd(cmd->cmd[0], args, args);
-		return (ft_free_matrix(args), 1);
-	}
-	return (0);
-}
-void	exec_child(t_cmds *cmd)	
+void	exec_child(t_cmds *cmd)
 {
 	char	**cleaned;
 	char	**updated_envs;
@@ -36,13 +25,10 @@ void	exec_child(t_cmds *cmd)
 	signal(SIGINT, handle_sigint);
 	cleaned = array_to_exec(cmd);
 	updated_envs = list_to_char(*get_env_list());
-
 	if (has_redir(cmd))
 		exec_redirections(cmd);
-
 	if (has_builtin(cmd))
 		exec_builtin(cleaned[0], cleaned, updated_envs);
-		
 	if (is_system_path_command(cleaned[0], updated_envs))
 		exec_path(cleaned[0], cleaned, updated_envs);
 	if (super_checker_goated(cmd, cleaned[0], updated_envs) != 0)
