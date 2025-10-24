509038b4415027e37c9fd2c8d8d34d75019002a0
jlima <jlima-so@c2r5s11.42lisboa.com>
2025-10-24 16:58:50 +0100

    fixed quotes

--- parse/expansions.c
+++ parse/expansions.c
@@ -6,7 +6,7 @@
 /*   By: jlima-so <jlima-so@student.42lisba.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 11:35:14 by namejojo          #+#    #+#             */
-/*   Updated: 2025/10/24 16:44:15 by jlima-so         ###   ########.fr       */
+/*   Updated: 2025/10/24 16:58:03 by jlima-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,14 +54,13 @@ char	*expand_hd(char *str)
 	return (str);
 }
 
-void	quote_aux(char **s, int *count, char **str2)
+void	quote_aux(char **s, int *count, char **str2, int value)
 {
 	int		ind;
 	char	ch;
 
 	ch = (*s + *count)[0];
 	ind = 1;
-	printf("%s %d %d\n", *s, *count, ind);
 	while ((*s + *count)[ind] != '\0' && (*s + *count)[ind] != ch)
 	{
 		if (ch == '\"')
@@ -69,18 +68,13 @@ void	quote_aux(char **s, int *count, char **str2)
 			*s = single_expand(*s, *count + ind, 0);
 			if (*(*s + 2) == '\0')
 				break ;
-			printf("str=%s\nstr2=%s\n", *s + *count, *str2 + *count);
-			printf("%d\n", ind);
-			ind += get_diff(*s, *str2, 0);
-			printf("%d\n", ind);
+			value = get_diff(*s + ind + *count, *str2 + ind + *count, 0);
+			ind += value - 1 * (value != 0);
+			free(*str2);
+			*str2 = ft_strdup(*s);
 		}
 		ind += ((*s + *count)[ind] != '\0');
 	}
-	printf("%s\n", (*s + *count));
-	printf("%s\n", (*s + *count) + 1);
-	printf("%s\n", (*s + *count) + ind);
-	printf("%s\n", (*s + *count) + ind + 1);
-	printf("IND IS=%d\n", ind);
 	ft_memmove((*s + *count) + ind, (*s + *count) + ind + 1,
 		ft_strlen((*s + *count) + ind));
 	ft_memmove((*s + *count), (*s + *count) + 1, ft_strlen((*s + *count)));
@@ -103,13 +97,12 @@ char	*quote(char *str)
 			return (free(str2), str);
 		if (ft_strcmp(str, str2))
 		{
-			printf("dif\n%s\n%s\n", str, str2);
 			count += get_diff(str + count, str2 + count + 1, 0);
 			free(str2);
 			str2 = ft_strdup(str);
 		}
 		if (*(str + count) == '\"' || *(str + count) == '\'')
-			quote_aux(&str, &count, &str2);
+			quote_aux(&str, &count, &str2, 0);
 		else
 			count += (str[count] != '\0');
 	}
